74c6958e073bcead451fb2d010b3e3711ed27c3e
ferielziani <ferielziani1607@gmail.com>
2025-11-12 23:31:56 -0500

    Creation des 3 instances et affichage de nombres total des taches

--- exe7.dart
+++ exe7.dart
@@ -5,3 +5,10 @@ class Tache{
     nombreTotal++;
   }
 }
+void main (){
+  Tache t1 = Tache("Faire les courses");
+  Tache t2 = Tache("Nettoyer la maison");
+  Tache t3 = Tache("Préparer le dîner");
+
+  print("Nombre total de tâches créées : ${Tache.nombreTotal}"); 
+}
