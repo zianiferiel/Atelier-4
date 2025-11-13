a2fe8593c15dd3a77884e742cab71e3fcc979acf
ferielziani <ferielziani1607@gmail.com>
2025-11-13 00:23:30 -0500

    Q8 à Q9

--- exe8.dart
+++ exe8.dart
@@ -1,30 +1,33 @@
 class Livre{
   String titre;
   String auteur;
-  int _pages=200;
+  int _pages=0;
   static int totalLivres=0;
-  Livre(this.titre, this.auteur){
+  Livre(this.titre, this.auteur,[int pages =0 ] ){
+    _pages = pages;
     totalLivres++;
   }
   int get pages => _pages;
 
   void afficherInfos(){
-    print("Titre: $titre, Auteur: $auteur");
+    print("Titre: $titre, Auteur: $auteur, Pages: $_pages");
+  }
+}
+class Roman extends Livre{
+  String genre;
+  Roman(String titre, String auteur, this.genre, [int pages = 0]) : super(titre, auteur, pages);
+  @override
+  void afficherInfos(){
+    print("Titre: $titre, Auteur: $auteur, Genre: $genre, Pages: $pages");
   }
 }
 void main (){
-  Livre l1 = Livre("1984", "George Orwell");
-  Livre l2 = Livre("Le Petit Prince", "Antoine de Saint-Exupéry");
-  Livre l3 = Livre("la Peste", "Albert Camus");
-  Livre l4 = Livre("Les Misérables", "Victor Hugo");
-  Livre l5 = Livre("Candide", "Voltaire");
-  Livre l6 = Livre("Germinal", "Émile Zola");
+  Livre l1 = Livre("1984", "George Orwell",200);
+  Livre l2 = Livre("Le Petit Prince", "Antoine de Saint-Exupéry",96);
+  Livre l3 = Livre("la Peste", "Albert Camus",308);
   l1.afficherInfos();
   l2.afficherInfos();
   l3.afficherInfos();
-  l4.afficherInfos();
-  l5.afficherInfos();
-  l6.afficherInfos();
   print("Nombre total de livres créés : ${Livre.totalLivres}");
 }
 
