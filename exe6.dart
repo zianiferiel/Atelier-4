d7bf044d969b9205ccbb856ecc6f76ba055fae5e
ferielziani <ferielziani1607@gmail.com>
2025-11-12 23:12:00 -0500

    création de la classe ServerAPI

--- exe6.dart
+++ exe6.dart
@@ -2,3 +2,12 @@ abstract class Connectable {
   void connect(String utilisateur);
   void disconnect();
 }
+class ServerAPI implements Connectable {
+  @override
+  void connect(String utilisateur) {
+    print('Server API: Connexion établie pour l\'utilisateur $utilisateur.');}
+    @override
+    void disconnect() {
+      print('Server API: Déconnexion réussi.');
+    }
+  }
