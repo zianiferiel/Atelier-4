9468f10ea434d08f942495a0c45190d047d8abf3
ferielziani <ferielziani1607@gmail.com>
2025-11-12 23:18:08 -0500

    Creation des instance ServerAPI,BaseDonnees et la list Connectable

--- exe6.dart
+++ exe6.dart
@@ -11,3 +11,21 @@ class ServerAPI implements Connectable {
       print('Server API: Déconnexion réussi.');
     }
   }
+  class BaseDonnee implements Connectable {
+    @override
+    void connect(String utilisateur) {
+      print('Base de Données: Connexion établie pour l\'utilisateur $utilisateur.');}
+      @override
+      void disconnect() {
+        print('Base de Données: Déconnexion réussi.');
+      }
+    }
+    void main() {
+      var api = ServerAPI();
+      var db = BaseDonnee();
+      List<Connectable> services = [api, db];
+      for (var s in services) {
+        s.connect('admin');
+        s.disconnect();
+      }
+    }
