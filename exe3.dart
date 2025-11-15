5414bca23c0caf898e85e63f94334004c20fa6f3
ferielziani <ferielziani1607@gmail.com>
2025-11-14 22:19:46 -0500

    Q6

--- exe3.dart
+++ exe3.dart
@@ -3,3 +3,11 @@ class Compte {
   final String numeroCompte;
   Compte(this.numeroCompte,[ this._solde = 0.0]);
 }
+String get solde => "${_solde.toStringAsFixed(2)}\$";
+void depot (double montant){
+  if (montant > 0){
+    _solde += montant;
+    else {
+      print("Montant de depot invalide.");
+  } 
+}
